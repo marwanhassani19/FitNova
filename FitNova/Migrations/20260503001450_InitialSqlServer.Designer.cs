ee4817542e7ecf1cc8fcb91057adad2be3e7f70e
hassani marwan <146874724+marwanhassani19@users.noreply.github.com>
2026-05-04 20:30:34 +0200

    Add files via upload

--- FitNova/Migrations/20260503001450_InitialSqlServer.Designer.cs
+++ FitNova/Migrations/20260503001450_InitialSqlServer.Designer.cs
@@ -4,6 +4,7 @@ using FitNova.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FitNova.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260503001450_InitialSqlServer")]
+    partial class InitialSqlServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
